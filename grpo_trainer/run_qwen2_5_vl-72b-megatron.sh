22ff7a1ac41528293e8b5f9369b2a03d46c79938
Liwei Ma <liweim@nvidia.com>
2025-09-25 10:46:54 +0800

    7b running, 72b fsdp running, mcore oom

--- grpo_trainer/run_qwen2_5_vl-72b-megatron.sh
+++ grpo_trainer/run_qwen2_5_vl-72b-megatron.sh
@@ -3,7 +3,7 @@ ENGINE=${1:-vllm}
 export CUDA_DEVICE_MAX_CONNECTIONS=1 # For megatron communication/computation overlapping
 
 
-HF_MODEL_PATH=${HF_MODEL_PATH:-"/data/Qwen2.5-VL-7B-Instruct"}
+HF_MODEL_PATH=${HF_MODEL_PATH:-"/data/Qwen2.5-VL-72B-Instruct"}
 
 ALL_OFFLOAD=${ALL_OFFLOAD:-True}
 COMMON_PARAM_OFFLOAD=${COMMON_PARAM_OFFLOAD:-$ALL_OFFLOAD}
@@ -14,10 +14,10 @@ ACTOR_PARAM_OFFLOAD=${ACTOR_PARAM_OFFLOAD:-$COMMON_PARAM_OFFLOAD}
 ACTOR_GRAD_OFFLOAD=${ACTOR_GRAD_OFFLOAD:-$COMMON_GRAD_OFFLOAD}
 ACTOR_OPTIMIZER_OFFLOAD=${ACTOR_OPTIMIZER_OFFLOAD:-$COMMON_OPTIMIZER_OFFLOAD}
 REF_PARAM_OFFLOAD=${REF_PARAM_OFFLOAD:-$COMMON_PARAM_OFFLOAD}
-TURN=${TURN:-1}
 
 verl_version=$(git rev-parse --short=4 HEAD)
-experiment_name=${experiment_name:-7b-mcore-vllm-n4-${verl_version}}
+nnodes=${nnodes:-8}
+experiment_name=${experiment_name:-72b-mcore-n${nnodes}-${verl_version}}
 
 train_path=$HOME/data/geo3k/train.parquet
 test_path=$HOME/data/geo3k/test.parquet
@@ -38,28 +38,26 @@ verl_args=(
     actor_rollout_ref.actor.optim.lr=1e-6
     actor_rollout_ref.actor.ppo_mini_batch_size=64
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1
-    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=1
+    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=16
     actor_rollout_ref.actor.megatron.tensor_model_parallel_size=4
     actor_rollout_ref.actor.use_kl_loss=True
     actor_rollout_ref.actor.kl_loss_coef=0.01
     actor_rollout_ref.actor.kl_loss_type=low_var_kl
     actor_rollout_ref.actor.entropy_coeff=0
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=20
+    actor_rollout_ref.rollout.tensor_model_parallel_size=8
     actor_rollout_ref.actor.use_dynamic_bsz=True
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=5120
     actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True
     actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=20480
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=20
-    actor_rollout_ref.rollout.tensor_model_parallel_size=4
     actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=20480
     actor_rollout_ref.rollout.name=$ENGINE
     +actor_rollout_ref.rollout.engine_kwargs.vllm.disable_mm_preprocessor_cache=True
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.4
-    actor_rollout_ref.rollout.max_model_len=32768
-    actor_rollout_ref.rollout.max_num_batched_tokens=32768
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5
     actor_rollout_ref.rollout.n=5
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=20
-    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=1
+    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=16
     actor_rollout_ref.ref.megatron.tensor_model_parallel_size=4
     actor_rollout_ref.actor.megatron.use_mbridge=True
     actor_rollout_ref.actor.megatron.param_offload=${ACTOR_PARAM_OFFLOAD}
@@ -72,11 +70,10 @@ verl_args=(
     trainer.project_name='verl_grpo_example_geo3k'
     trainer.experiment_name=$experiment_name
     trainer.n_gpus_per_node=8
-    trainer.nnodes=4
-    trainer.resume_mode=disable
+    trainer.nnodes=$nnodes
     trainer.save_freq=20
     trainer.test_freq=5
     trainer.total_epochs=15
 )
 
-python3 -m verl.trainer.main_ppo ${verl_args[@]} $@ | tee -a ${experiment_name}-${TURN}.log
+python3 -m verl.trainer.main_ppo ${verl_args[@]} $@ | tee -a ${experiment_name}.log
