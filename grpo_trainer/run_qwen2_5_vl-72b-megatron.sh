151b5db602fcf02468de9f0dd62885b4b424d16e
Liwei Ma <liweim@nvidia.com>
2025-09-28 10:53:00 +0800

    try 32b, init dapo

--- grpo_trainer/run_qwen2_5_vl-72b-megatron.sh
+++ grpo_trainer/run_qwen2_5_vl-72b-megatron.sh
@@ -14,10 +14,12 @@ ACTOR_PARAM_OFFLOAD=${ACTOR_PARAM_OFFLOAD:-$COMMON_PARAM_OFFLOAD}
 ACTOR_GRAD_OFFLOAD=${ACTOR_GRAD_OFFLOAD:-$COMMON_GRAD_OFFLOAD}
 ACTOR_OPTIMIZER_OFFLOAD=${ACTOR_OPTIMIZER_OFFLOAD:-$COMMON_OPTIMIZER_OFFLOAD}
 REF_PARAM_OFFLOAD=${REF_PARAM_OFFLOAD:-$COMMON_PARAM_OFFLOAD}
+optimizer_offload_fraction=${OFFLOAD_FRACTION:-1.0}
 
 verl_version=$(git rev-parse --short=4 HEAD)
-nnodes=${nnodes:-8}
-experiment_name=${experiment_name:-72b-mcore-n${nnodes}-${verl_version}}
+NNODES=${NNODES:-8}
+TURN=${TURN:-1}
+experiment_name=${experiment_name:-72b-mcore-n${NNODES}-${verl_version}-${TURN}}
 
 train_path=$HOME/data/geo3k/train.parquet
 test_path=$HOME/data/geo3k/test.parquet
@@ -38,8 +40,8 @@ verl_args=(
     actor_rollout_ref.actor.optim.lr=1e-6
     actor_rollout_ref.actor.ppo_mini_batch_size=64
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1
-    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=16
-    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=4
+    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=32
+    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=8
     actor_rollout_ref.actor.use_kl_loss=True
     actor_rollout_ref.actor.kl_loss_coef=0.01
     actor_rollout_ref.actor.kl_loss_type=low_var_kl
@@ -57,12 +59,17 @@ verl_args=(
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5
     actor_rollout_ref.rollout.n=5
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=20
-    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=16
-    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=4
+    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=32
+    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=8
     actor_rollout_ref.actor.megatron.use_mbridge=True
     actor_rollout_ref.actor.megatron.param_offload=${ACTOR_PARAM_OFFLOAD}
     actor_rollout_ref.actor.megatron.optimizer_offload=${ACTOR_OPTIMIZER_OFFLOAD}
     actor_rollout_ref.actor.megatron.grad_offload=${ACTOR_GRAD_OFFLOAD}
+    +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_offload_fraction=${optimizer_offload_fraction}
+    +actor_rollout_ref.actor.optim.override_optimizer_config.overlap_cpu_optimizer_d2h_h2d=True
+    +actor_rollout_ref.actor.optim.override_optimizer_config.use_precision_aware_optimizer=True
+    +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_cpu_offload=True
+    #+actor_rollout_ref.actor.megatron.override_transformer_config.num_layers_in_first_pipeline_stage=3
     actor_rollout_ref.ref.megatron.param_offload=${REF_PARAM_OFFLOAD}
     algorithm.use_kl_in_reward=False
     trainer.critic_warmup=0
@@ -70,10 +77,10 @@ verl_args=(
     trainer.project_name='verl_grpo_example_geo3k'
     trainer.experiment_name=$experiment_name
     trainer.n_gpus_per_node=8
-    trainer.nnodes=$nnodes
+    trainer.nnodes=$NNODES
     trainer.save_freq=20
     trainer.test_freq=5
     trainer.total_epochs=15
 )
 
-python3 -m verl.trainer.main_ppo ${verl_args[@]} $@ | tee -a ${experiment_name}.log
+python3 -m verl.trainer.main_ppo ${verl_args[@]} $@ 2>&1 | tee -a ${experiment_name}.log
